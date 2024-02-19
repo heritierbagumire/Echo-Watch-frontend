5c603d848d8ff0cf97959e64adf10e68cdf29d13
heritierbagumire <heritierbagumire367@gmail.com>
2024-02-19 16:44:50 +0200

    initializing project

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Typography variant="h4" className="text-white text-center ">Hello dashboard page is here </Typography>
+  );
+}
